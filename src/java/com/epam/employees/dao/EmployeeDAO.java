b34f4bcd32d7dc70a431798b9b0624c38678812b
Nickolay_Petrash <Nickolay_Petrash@EPBYMINW2290.minsk.epam.com>
2013-02-01 01:20:55 +1200

    Pagination

--- src/java/com/epam/employees/dao/EmployeeDAO.java
+++ src/java/com/epam/employees/dao/EmployeeDAO.java
@@ -7,17 +7,12 @@ package com.epam.employees.dao;
 import com.epam.employees.model.PersistentEntity;
 import com.epam.employees.pagination.page.Page;
 import com.epam.employees.pagination.page.PageRequest;
-import java.util.List;
 
 /**
  *
  * @author Owl
  */
-public interface IEmployeeDAO<Entity extends PersistentEntity> {
-
-   // public List<T> getRecords(int pageSize, int pageNumber);
-
-    List<Entity> findByNamedQuery(String queryName, Object... params);
+public interface EmployeeDAO<Entity extends PersistentEntity> {
 
     Page<Entity> findByNamedQuery(PageRequest pageRequest, String queryName, Object... params);
 }
