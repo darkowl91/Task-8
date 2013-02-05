ec43e76014083f29422a94b4f4b9883b054f5be4
Nickolay Petrash <darkowl91@gmail.com>
2013-02-06 00:42:00 +0300

    JDBC dao

--- src/java/com/epam/employees/dao/JdbcImpl.java
+++ src/java/com/epam/employees/dao/JdbcImpl.java
@@ -3,6 +3,7 @@ package com.epam.employees.dao;
 import com.epam.employees.model.PersistentEntity;
 import com.epam.employees.pagination.page.Page;
 import com.epam.employees.util.ConnectionPool;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,10 +15,11 @@ import java.sql.SQLException;
 public final class JdbcImpl<Entity extends PersistentEntity> implements
         PersistentEntityDAO<Entity> {
 
-     private ConnectionPool connectionPool;
-     private Releaser resourceReleaser;
-     private PreparedStatement preparedStatement = null;
-     private ResultSet resultSet = null;
+    private ConnectionPool connectionPool;
+    private Releaser resourceReleaser;
+    private Connection connection;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
 
     public void setConnectionPool(ConnectionPool connectionPool) {
         this.connectionPool = connectionPool;
@@ -26,10 +28,19 @@ public final class JdbcImpl<Entity extends PersistentEntity> implements
     public void setResourceReleaser(Releaser resourceReleaser) {
         this.resourceReleaser = resourceReleaser;
     }
-    
-    
+
     @Override
     public Page<Entity> findByNamedQuery(int pageNumber, int pageSize, String queryName, Object... params) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Page page = null;
+        try {
+            connection = connectionPool.getConnection();
+
+        } catch (Exception e) {
+            //
+        } finally {
+            resourceReleaser.closeAll(preparedStatement, resultSet);
+            resourceReleaser.takeConnectionBack(connectionPool, connection);
+        }
+        return page;
     }
 }
