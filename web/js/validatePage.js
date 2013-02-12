7fff3aca221336cff153080fb7c2020186fd5a8d
Nickolay_Petrash <Nickolay_Petrash@EPBYMINW2290.minsk.epam.com>
2013-02-12 20:41:08 +1200

    validate js

--- web/js/validatePage.js
+++ web/js/validatePage.js
@@ -1,6 +1,4 @@
-var dateRegexp = new RegExp("((0[1-9])|([1-2][0-9])|30|31)-((0[1-9])|(1[0-2]))-(19|20)[0-9]{2}");
-var modelRegexp = new RegExp("[A-Za-zÀ-ßà-ÿ¨¸]{2}\\d{3}");
-var priceRegexp = new RegExp("[0-9]+(?:\.[0-9]+)?");
+var pageRegexp = new RegExp("[0-9]+(?:\.[0-9]+)?");
 
 function validate(element) {
     var hasError = false;
@@ -14,11 +12,18 @@ function validate(element) {
             if (strLenght == 0) {
                 message = emptyField;  	
                 hasError = true;
+            } else if(strLenght != 0 && !pageRegexp.test(element.value)) {
+                message= incorrectFormat;
+                hasError = true;
             }
             break;
+            
         case "pageNumber":
             if (strLenght == 0) {
                 message = emptyField;
+                hasError = true;
+            } else if(strLenght != 0 && !pageRegexp.test(element.value)) {
+                message= incorrectFormat;
                 hasError = true;
             }
             break;
