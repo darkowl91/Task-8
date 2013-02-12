80a7ec19873f69fd7e0652ffdde894387fdeffe6
Nickolay_Petrash <Nickolay_Petrash@EPBYMINW2290.minsk.epam.com>
2013-02-12 19:07:29 +1200

    Tag

--- src/java/com/epam/employees/pagination/tag/PagingTag.java
+++ src/java/com/epam/employees/pagination/tag/PagingTag.java
@@ -25,13 +25,13 @@ public class PagingTag extends TagSupport {
 
     @Override
     public int doStartTag() throws JspException {
-        try {
+         try {
             JspWriter out = pageContext.getOut();
             printForm(out);
         } catch (IOException ex) {
             Logger.getLogger(PagingTag.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return SKIP_BODY;
+        return super.doStartTag();
     }
 
     public int getPageNumber() {
@@ -67,35 +67,20 @@ public class PagingTag extends TagSupport {
     }
 
     private void printForm(JspWriter out) throws IOException {
-        out.write("<form style=\"navbar-form pull-right\" action=\"" + action + "\"onsubmit=\"return validatePagingForm(this);\">");
+        out.write("<form class=\"navbar-form pull-right\" action=\"" + action + "\"onsubmit=\"return validatePagingForm(this);\">");
         out.write("<span class=\"help-inline\">");
         out.write("Size");
         out.write("</span>");
-        out.write("<input type=\"text\" style=\" span2\" name=\"employeesOnPage\" size=\"3\" value=\"" + pageSize + "\" />");
+        out.write("<input type=\"text\" class=\" span2\" name=\"pageSize\" size=\"3\" value=\"" + pageSize + "\" />");
         out.write("<span class=\"help-inline\">");
         out.write("Page");
         out.write("</span>");
-        out.write("<input type=\"text\" style=\" span2\" name=\"page\" size=\"3\" value=\"" + pageNumber + "\" />");
+        out.write("<input type=\"text\" class=\" span2\" name=\"pageNumber\" size=\"3\" value=\"" + pageNumber + "\" />");
         out.write("<span class=\"help-inline\">");
         out.write("of " + totalPages);
         out.write("</span>");
         out.write("<input type=\"submit\" class=\"btn\" value=\"GO!\"/>");
         out.write("</form>");
     }
-//<html:form action="/EmployeeList" styleClass="navbar-form pull-right">
-//<html:hidden property="method" value="EmployeeList.do" />
-//<span class="help-inline">
-//<bean:message key="header.size"/>
-//</span>
-//<nested:text name="EmployeesForm" property="pageSize" styleClass="span2"/>
-//<span class="help-inline">
-//<nested:message key="header.page"/>
-//</span>
-//<nested:text name="EmployeesForm" property="pageNumber" styleClass="span2"/>
-//<span class="help-inline">
-//<bean:message key="header.of"/>
-//${EmployeesForm.totalPages}
-//</span>
-//<input type="submit" value="GO!" class="btn" />
-//</html:form>
+
 }
